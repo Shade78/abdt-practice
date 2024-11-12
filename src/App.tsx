49172585c54a6bb3c5745f9ddb25abd168db5164
Shade78 <shademoon322@gmail.com>
2024-11-12 17:31:15 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,151 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import "./App.css";
+
+const formSchema = z.object({
+  fullName: z
+    .string()
+    .min(3, "Пожалуйста, введите ваше ФИО")
+    .max(50)
+    .regex(/\s/, "Вы не указали полное ФИО"),
+
+  phoneNumber: z
+    .string()
+    .regex(/^\d+$/)
+    .min(10, "Номер телефона должен содержать 10 цифр"),
+  city: z
+    .string()
+    .regex(/[a-zA-Z]+$/)
+    .min(1, "Введите корректное название города"),
+  street: z
+    .string()
+    .regex(/[a-zA-Z]+$/)
+    .min(1, "Введите корректное название улицы"),
+  apartmentNumber: z
+    .string()
+    .regex(/^[^a-z]*$/, "Введите только номер квартиры")
+    .min(1),
+});
+
+type TFormSchema = z.infer<typeof formSchema>;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isCourierDelivery, setIsCourierDelivery] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+    getValues,
+  } = useForm<TFormSchema>({
+    defaultValues: {
+      fullName: "",
+      phoneNumber: "",
+      city: "",
+      street: "",
+    },
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = async (data: TFormSchema) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    console.log(data);
+
+    reset();
+  };
 
   return (
-    <>
+    <div className="container">
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setIsCourierDelivery(false)}>
+          Личные данные
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button onClick={() => setIsCourierDelivery(true)}>
+          Курьерская доставка
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <form onSubmit={handleSubmit(onSubmit)} action="#">
+        {!isCourierDelivery && (
+          <div className="form-container">
+            <input
+              className={errors.fullName ? "input-error" : ""}
+              type="text"
+              placeholder="ФИО"
+              {...register("fullName", {
+                required: "Обязательное поле",
+                // validate: (value) => {
+                //   console.log("errors fullname", errors.fullName);
+                //   return /\s/.test(value) || "error message";
+                // },
+              })}
+            />
+            {errors.fullName && <p>{`${errors?.fullName.message}`}</p>}
+            <input
+              type="tel"
+              placeholder="Номер телефона"
+              {...register("phoneNumber", {
+                required: "Обязательное поле",
+              })}
+            />
+            {errors.phoneNumber && <p>{`${errors?.phoneNumber.message}`}</p>}
+            <input
+              type="text"
+              placeholder="Город проживания"
+              {...register("city", {
+                required: "Обязательное поле",
+              })}
+            />
+            {errors.city && <p>{`${errors?.city.message}`}</p>}
+            <input
+              // type="text"
+              placeholder="Улица"
+              {...register("street", {
+                required: "Обязательное поле",
+              })}
+            />
+            {errors.street && <p>{`${errors.street?.message}`}</p>}
+            <input
+              // type="number"
+              placeholder="Номер квартиры"
+              {...register("apartmentNumber", {
+                required: "Обязательное поле",
+              })}
+            />
+            {errors.apartmentNumber && (
+              <p>{`${errors.apartmentNumber?.message}`}</p>
+            )}
+          </div>
+        )}
+        {isCourierDelivery && (
+          <div className="delivery">
+            <input
+              type="tel"
+              placeholder="Номер телефона"
+              {...register("phoneNumber", {
+                required: "Обязательное поле",
+              })}
+            />
+            {errors.phoneNumber && <p>{`${errors.phoneNumber?.message}`}</p>}
+            <input
+              type="text"
+              placeholder="Город проживания"
+              {...register("city", {
+                required: "Обязательное поле",
+              })}
+            />
+            {errors.city && <p>{`${errors.city?.message}`}</p>}
+          </div>
+        )}
+        <button disabled={isSubmitting} type="submit">
+          Отправить
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
