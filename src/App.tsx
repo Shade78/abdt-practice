64d5e22ade9f5b5cbf999ac2fbc3208dde9e409f
Shade78 <shademoon322@gmail.com>
2024-11-13 18:15:44 +0300

    test2

--- src/App.tsx
+++ src/App.tsx
@@ -1,150 +1,200 @@
 import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Input, InputNumber } from "antd";
+
+import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
+
 import "./App.css";
 
-const formSchema = z.object({
+const fullSchema = z.object({
   fullName: z
-    .string()
-    .min(3, "Пожалуйста, введите ваше ФИО")
-    .max(50)
-    .regex(/\s/, "Вы не указали полное ФИО"),
-
+    .string({
+      invalid_type_error: "Invalid name",
+      required_error: "Обязательное поле",
+    })
+    .min(3),
   phoneNumber: z
-    .string()
-    .regex(/^\d+$/)
+    .string({
+      invalid_type_error: "Invalid name",
+      required_error: "Обязательное поле",
+    })
     .min(10, "Номер телефона должен содержать 10 цифр"),
   city: z
-    .string()
-    .regex(/[a-zA-Z]+$/)
+    .string({
+      invalid_type_error: "Invalid name",
+      required_error: "Обязательное поле",
+    })
     .min(1, "Введите корректное название города"),
   street: z
-    .string()
-    .regex(/[a-zA-Z]+$/)
+    .string({
+      invalid_type_error: "Invalid name",
+      required_error: "Обязательное поле",
+    })
     .min(1, "Введите корректное название улицы"),
   apartmentNumber: z
-    .string()
-    .regex(/^[^a-z]*$/, "Введите только номер квартиры")
+    .number({
+      invalid_type_error: "Invalid name11",
+      required_error: "Обязательное поле",
+    })
     .min(1),
 });
 
-type TFormSchema = z.infer<typeof formSchema>;
+const courierSchema = z.object({
+  phoneNumber2: z
+    .string({
+      invalid_type_error: "Invalid name",
+      required_error: "Обязательное поле",
+    })
+    .min(10, "Номер телефона должен содержать 10 цифр"),
+  city2: z
+    .string({
+      invalid_type_error: "Invalid name",
+      required_error: "Обязательное поле",
+    })
+    .min(1, "Введите корректное название города"),
+});
+
+type TFullSchema = z.infer<typeof fullSchema>;
+type TCourierSchema = z.infer<typeof courierSchema>;
+
+type TFormData = TCourierSchema | TFullSchema;
 
 function App() {
   const [isCourierDelivery, setIsCourierDelivery] = useState(false);
 
   const {
-    register,
     handleSubmit,
-    formState: { errors, isSubmitting },
     reset,
-    getValues,
-  } = useForm<TFormSchema>({
-    defaultValues: {
-      fullName: "",
-      phoneNumber: "",
-      city: "",
-      street: "",
-    },
-    resolver: zodResolver(formSchema),
+    formState: { errors },
+    control,
+  } = useForm<TFullSchema>({
+    resolver: zodResolver(fullSchema),
   });
 
-  const onSubmit = async (data: TFormSchema) => {
+  const {
+    handleSubmit: handleSubmit2,
+    reset: reset2,
+    formState: { errors: errors2 },
+    control: control2,
+  } = useForm<TCourierSchema>({
+    resolver: zodResolver(courierSchema),
+  });
+
+  const onSubmitFull = async (data: TFullSchema) => {
     await new Promise((resolve) => setTimeout(resolve, 1000));
     console.log(data);
 
     reset();
   };
 
+  const onSubmitCourier = async (data: TCourierSchema) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    console.log(data);
+
+    reset2();
+  };
+
   return (
-    <div className="container">
+    <>
       <div>
-        <button onClick={() => setIsCourierDelivery(false)}>
+        <button
+          className="header-button"
+          onClick={() => setIsCourierDelivery(false)}
+        >
           Личные данные
         </button>
-        <button onClick={() => setIsCourierDelivery(true)}>
+        <button
+          className="header-button"
+          onClick={() => setIsCourierDelivery(true)}
+        >
           Курьерская доставка
         </button>
       </div>
 
-      <form onSubmit={handleSubmit(onSubmit)} action="#">
+      <form onSubmit={handleSubmit(onSubmitFull)} action="#">
         {!isCourierDelivery && (
-          <div className="form-container">
-            <input
-              className={errors.fullName ? "input-error" : ""}
-              type="text"
-              placeholder="ФИО"
-              {...register("fullName", {
-                required: "Обязательное поле",
-                // validate: (value) => {
-                //   console.log("errors fullname", errors.fullName);
-                //   return /\s/.test(value) || "error message";
-                // },
-              })}
+          <div className="controllers-container">
+            <Controller
+              control={control}
+              name="fullName"
+              render={({ field }) => <Input placeholder="ФИО" {...field} />}
             />
             {errors.fullName && <p>{`${errors?.fullName.message}`}</p>}
-            <input
-              type="tel"
-              placeholder="Номер телефона"
-              {...register("phoneNumber", {
-                required: "Обязательное поле",
-              })}
+            <Controller
+              control={control}
+              name="phoneNumber"
+              render={({ field }) => (
+                <PhoneInput
+                  defaultCountry="RU"
+                  placeholder="Номер телефона"
+                  {...field}
+                />
+              )}
             />
             {errors.phoneNumber && <p>{`${errors?.phoneNumber.message}`}</p>}
-            <input
-              type="text"
-              placeholder="Город проживания"
-              {...register("city", {
-                required: "Обязательное поле",
-              })}
+            <Controller
+              control={control}
+              name="city"
+              render={({ field }) => <Input placeholder="Город" {...field} />}
             />
             {errors.city && <p>{`${errors?.city.message}`}</p>}
-            <input
-              // type="text"
-              placeholder="Улица"
-              {...register("street", {
-                required: "Обязательное поле",
-              })}
+            <Controller
+              control={control}
+              name="street"
+              render={({ field }) => <Input placeholder="Улица" {...field} />}
             />
-            {errors.street && <p>{`${errors.street?.message}`}</p>}
-            <input
-              // type="number"
-              placeholder="Номер квартиры"
-              {...register("apartmentNumber", {
-                required: "Обязательное поле",
-              })}
+            {errors.street && <p>{`${errors?.street.message}`}</p>}
+            <Controller
+              control={control}
+              name="apartmentNumber"
+              render={({ field }) => (
+                <InputNumber
+                  placeholder="Номер дома"
+                  className="apartmentNumber-input"
+                  {...field}
+                />
+              )}
             />
+
             {errors.apartmentNumber && (
-              <p>{`${errors.apartmentNumber?.message}`}</p>
+              <p>{`${errors?.apartmentNumber.message}`}</p>
             )}
+            <button type="submit">Отправить</button>
           </div>
         )}
-        {isCourierDelivery && (
-          <div className="delivery">
-            <input
-              type="tel"
-              placeholder="Номер телефона"
-              {...register("phoneNumber", {
-                required: "Обязательное поле",
-              })}
-            />
-            {errors.phoneNumber && <p>{`${errors.phoneNumber?.message}`}</p>}
-            <input
-              type="text"
-              placeholder="Город проживания"
-              {...register("city", {
-                required: "Обязательное поле",
-              })}
-            />
-            {errors.city && <p>{`${errors.city?.message}`}</p>}
-          </div>
-        )}
-        <button disabled={isSubmitting} type="submit">
-          Отправить
-        </button>
       </form>
-    </div>
+
+      <form onSubmit={handleSubmit2(onSubmitCourier)} action="#">
+        {isCourierDelivery && (
+          <div className="controllers-container">
+            <Controller
+              control={control2}
+              name="phoneNumber2"
+              render={({ field }) => (
+                <PhoneInput
+                  defaultCountry="RU"
+                  placeholder="Номер телефона"
+                  {...field}
+                />
+              )}
+            />
+            {errors2.phoneNumber2 && (
+              <p>{`${errors2?.phoneNumber2.message}`}</p>
+            )}
+            <Controller
+              control={control2}
+              name="city2"
+              render={({ field }) => <Input placeholder="Город" {...field} />}
+            />
+            {errors2.city2 && <p>{`${errors2?.city2.message}`}</p>}
+            <button type="submit">Отправить</button>
+          </div>
+        )}
+      </form>
+    </>
   );
 }
 
